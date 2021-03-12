474c0330a298d9372c3e80d9717bf72a44b0358d
Nagy Evelin <evelinnagy11@gmail.com>
2021-03-12 22:14:35 +0100

    Added GeoLocation record class and fix a typo in jvm.config file

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,7 @@
+package geolocator;
+
+public record GeoLocation(String as, String city, String country, String countrCode, String isp, double lat, double lon,
+        String org, String query, String region, String regionName, String timezone, String zip) {
+
+        }
+
